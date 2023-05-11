e623194d505405ca21dfab9ca323251e7f332cd6
Michiel <97068201@st.deltion.nl>
2023-05-11 17:51:26 +0200

    Michiel

--- navbar/navbar.js
+++
@@ -1,6 +0,0 @@
-const navbarHamburger = document.querySelector(".navbar-hamburger");
-const navbarMenu = document.querySelector(".navbar-menu");
-
-navbarHamburger.addEventListener("click", function() {
-  navbarMenu.classList.toggle("active");
-});
